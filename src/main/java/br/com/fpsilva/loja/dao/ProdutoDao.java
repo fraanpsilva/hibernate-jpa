a6b17460b62445665fbcf96cda5e50d812bb6995
fraanpsilva <francilenesilva.pt@gmail.com>
2022-05-14 19:42:30 +0100

    realizando consultas com os métodos  finde realizando consultas com JPQL

--- src/main/java/br/com/fpsilva/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/fpsilva/loja/dao/ProdutoDao.java
@@ -3,6 +3,8 @@ package br.com.fpsilva.loja.dao;
 import br.com.fpsilva.loja.modelo.Produto;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class ProdutoDao {
 
@@ -15,4 +17,53 @@ public class ProdutoDao {
     public void cadastrar(Produto produto){
         this.em.persist(produto); // fazendo inserte na tabela de produto
     }
+
+    public Produto buscarPorId(Long id){
+        return em.find(Produto.class, id);
+    }
+
+    // utilizando JPQL
+    public List<Produto> buscarTodos(){
+        String jpql = "SELECT p FROM Produto p ";
+        return em.createQuery(jpql, Produto.class).getResultList();
+
+    }
+
+    // metodo com namedParameter
+    public List<Produto> buscarPorNome(String nomeProduto){
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nomeProduto)
+                .getResultList();
+
+    }
+
+    // metodo com interrogação
+    public List<Produto> buscarPorNome2(String nomeProduto){
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = ?1";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter(1, nomeProduto)
+                .getResultList();
+
+    }
+
+
+    // consulta com join (o join é automático)
+    public List<Produto> buscarPorNomeDaCategoria(String nomeCategoria){
+        String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nomeCategoria)
+                .getResultList();
+
+    }
+
+    // limitando dados em uma consulta
+    public BigDecimal buscarPrecoDoProdutoComNome(String nome){
+        String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, BigDecimal.class)
+                .setParameter("nome", nome)
+                .getSingleResult();
+
+    }
+
 }
--- src/test/java/br/com/fpsilva/loja/testes/CadastroDeProduto.java
+++ src/test/java/br/com/fpsilva/loja/testes/CadastroDeProduto.java
@@ -10,10 +10,37 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroDeProduto {
     public static void main(String[] args) {
 
+        cadastrarProduto();
+
+        // fazendo consultas no banco - ex 1
+        EntityManager em = JpaUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        Produto p = produtoDao.buscarPorId(1l);
+        System.out.println(p.getPreco());
+
+        // fazendo consulta - ex 2
+        List<Produto> todos = produtoDao.buscarTodos();
+        todos.forEach(p2 -> System.out.println(p.getNome()));
+
+        // fazendo consultas com filtros
+        List<Produto> todos2 = produtoDao.buscarPorNome("Xiaomi Redmi");
+        todos.forEach(p2 -> System.out.println(p.getNome()));
+
+        // fazendo consultas com filtros
+        List<Produto> todos3 = produtoDao.buscarPorNomeDaCategoria("CELULARES");
+        todos.forEach(p2 -> System.out.println(p.getNome()));
+
+        BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoComNome("Xiaomi Redmi");
+        System.out.println("Preço do produto: " + precoDoProduto);
+
+    }
+
+    private static void cadastrarProduto() {
         Categoria categoria1 = new Categoria("CELULARES");
         Produto p1 = new Produto("Xiaomi Redmi",
                 "Muito Legal", new BigDecimal("800"), categoria1);
@@ -28,6 +55,5 @@ public class CadastroDeProduto {
         em.getTransaction().commit(); // fazendo commit no banco
 
         em.close(); // fechando a conexão
-
     }
 }
