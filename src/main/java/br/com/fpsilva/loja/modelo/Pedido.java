55451657d46080c93d51286c9306c0ec6f87210a
fraanpsilva <francilenesilva.pt@gmail.com>
2022-05-15 23:39:58 +0100

    Relacionamento many-to-many (bidirecional)

--- src/main/java/br/com/fpsilva/loja/modelo/Pedido.java
+++ src/main/java/br/com/fpsilva/loja/modelo/Pedido.java
@@ -3,6 +3,8 @@ package br.com.fpsilva.loja.modelo;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_pedidos")
@@ -16,6 +18,23 @@ public class Pedido {
 
     @ManyToOne
     private Cliente cliente;
+
+    /* mapeamento manytomany simples, onde a tabela tem apenas duas colunas, das duas tabelas que ela referencia
+     * Quando temos um relacionamento manyToMany que precisa de mais de duas colunas, o ideal é criar uma nova
+     * entidade para representar essa tabela
+    */
+//    @ManyToMany
+//    @JoinTable(name = "itens_pedido")
+//    private List<Produto> produtos;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens = new ArrayList<>(); // boa prática, inicializar a lista de um relacionamento, para evitar ter que fazer muitas validações
+    /*
+    * Relacionamento bidirecional: relacionamento nas duas pontas
+    * Sempre que isso acontecer, temos que indicar que é um relacionamento bidirecional
+    * com o MappedBy="com o nome do atributo que tá do outro lado do relacionamento"
+    * */
+
 
     public Pedido() {
     }
@@ -54,5 +73,12 @@ public class Pedido {
 
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
+    }
+
+    // vinculando os dois lados do relacionamento - boa prática
+    public void adicionarItem(ItemPedido item){
+        item.setPedido(this);
+        this.itens.add(item);
+
     }
 }
